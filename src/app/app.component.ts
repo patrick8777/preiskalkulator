2a53f459d8275176af52ccd2fd9a84f948d18454
patrick8777 <patrick8777@gmail.com>
2023-02-28 14:46:48 +0100

    add money calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'preiskalkulator';
+  produkt1 = { checked: false, preis: 10 };
+  produkt2 = { checked: false, preis: 20 };
+  produkt3 = { checked: false, preis: 30 };
+  gesamtpreis = 0;
+
+  berechneGesamtpreis(): void {
+    this.gesamtpreis = 0;
+    if (this.produkt1.checked) {
+      this.gesamtpreis += this.produkt1.preis;
+    }
+    if (this.produkt2.checked) {
+      this.gesamtpreis += this.produkt2.preis;
+    }
+    if (this.produkt3.checked) {
+      this.gesamtpreis += this.produkt3.preis;
+    }
+  }
 }
+
